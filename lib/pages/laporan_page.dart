b712a8674051a69ceca0edb74b39dd7eaf947b9a
Dede Nugroho <dedenugrohomm2@gmail.com>
2023-06-13 22:49:01 +0700

    Membuat tampilan laporan keuangan

--- lib/pages/laporan_page.dart
+++ lib/pages/laporan_page.dart
@@ -1,14 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
-class LaporanPage extends StatelessWidget {
-  const LaporanPage({super.key});
+class LaporanPage extends StatefulWidget {
+  @override
+  _LaporanPageState createState() => _LaporanPageState();
+}
+
+class _LaporanPageState extends State<LaporanPage> {
+  List<Map<String, dynamic>> laporanKeuangan = [
+    {"tanggal": "01-01-2023", "jenis": "Masuk", "jumlah": 100000},
+    {"tanggal": "02-01-2023", "jenis": "Keluar", "jumlah": -50000},
+    {"tanggal": "03-01-2023", "jenis": "Keluar", "jumlah": -30000},
+    // tambahkan data laporan keuangan lainnya sesuai kebutuhan
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Laporan"),
+      appBar: AppBar(
+        title: Text('Laporan Keuangan'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DataTable(
+              columns: const [
+                DataColumn(label: Text('Tanggal')),
+                DataColumn(label: Text('Jenis')),
+                DataColumn(label: Text('Jumlah')),
+              ],
+              rows: laporanKeuangan
+                  .map(
+                    (data) => DataRow(
+                      cells: [
+                        DataCell(Text(data['tanggal'])),
+                        DataCell(Text(data['jenis'])),
+                        DataCell(Text(data['jumlah'].toString())),
+                      ],
+                    ),
+                  )
+                  .toList(),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Total Saldo: ${hitungTotalSaldo().toString()}',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  double hitungTotalSaldo() {
+    double totalSaldo = 0;
+    for (var data in laporanKeuangan) {
+      totalSaldo += data['jumlah'];
+    }
+    return totalSaldo;
   }
 }
